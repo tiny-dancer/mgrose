022c57bbccfa780ba96d5702ac898bd30c864198
tiny-dancer <grose.matthewf@gmail.com>
2018-03-11 16:21:28 -0500

    move to bootstrap npm

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import '../fonts/merriweather.css';
 //import '../fonts/linearicons/web-font/style.css';
 import '../fonts/et-line-font/style.css';
 import 'linearicons';
-import './bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'animate.css/animate.min.css';
 import './owl.carousel.min.css';
 import './owl.theme.default.min.css';
